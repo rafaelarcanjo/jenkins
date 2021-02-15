7695667b0e7b2b666dd7d4e9620e7341abd3505a
Rafael Arcanjo <rafael.wzs@gmail.com>
2021-02-15 11:31:46 -0300

    Corrigindo o volume do PHP

--- up.sh
+++ up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d --rm --name jenkins -u root -p 8080:8080 -v jenkins-data:/var/jenkins_home \
+docker run -d --rm --name jenkins -u root -p 8080:8080 -v /jenkins-data:/var/jenkins_home \
     -v $(which docker):/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean
 
 # Instalando o Docker Compose na imagem do Jenkins
